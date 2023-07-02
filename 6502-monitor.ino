21728ce15a186921b7e0d009962a7646bd2c78a4
Vishwa <vishwa181@gmail.com>
2023-07-02 12:19:03 +0530

    Add files via upload

---
+++ 6502-monitor.ino
@@ -0,0 +1,45 @@
+const char ADDR[] = {22, 23, 24, 25, 26, 27, 28, 29};
+const char DATA[] = {41,42, 43,44, 45, 46, 47, 48, 49};
+#define CLOCK 10
+#define READ_WRITE 3
+
+void setup() {
+  for (int n = 0; n < 16; n += 1) {
+    pinMode(ADDR[n], INPUT);
+  }
+  for (int n = 0; n < 8; n += 1) {
+    pinMode(DATA[n], INPUT);
+  }
+  pinMode(CLOCK, INPUT);
+  pinMode(READ_WRITE, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(CLOCK), onClock, RISING);
+  
+  Serial.begin(57600);
+}
+
+void onClock() {
+  char output[15];
+
+  unsigned int address = 0;
+  for (int n = 0; n < 16; n += 1) {
+    int bit = digitalRead(ADDR[n]) ? 1 : 0;
+    Serial.print(bit);
+    address = (address << 1) + bit;
+  }
+  
+  Serial.print("   ");
+  
+  unsigned int data = 0;
+  for (int n = 0; n < 8; n += 1) {
+    int bit = digitalRead(DATA[n]) ? 1 : 0;
+    Serial.print(bit);
+    data = (data << 1) + bit;
+  }
+
+  sprintf(output, "   %04x  %c %02x", address, digitalRead(READ_WRITE) ? 'r' : 'W', data);
+  Serial.println(output);  
+}
+
+void loop() {
+}
